012402f333782ca6e46608ef7e709f2acff29335
eost <eost@talend.com>
2020-06-20 15:40:01 -0400

    use maven tiles to compose property settings

---
+++ dynamic-endpoint/src/main/java/com/talend/se/hartfoder/dynamic_endpoint/EndpointFactoryRoute.java
@@ -0,0 +1,14 @@
+package com.talend.se.hartfoder.dynamic_endpoint;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EndpointFactoryRoute extends RouteBuilder {
+
+	@Override
+    public void configure() {
+        from("timer://world?fixedRate=true&period=1000")
+            .log("hello world");
+    }
+}
